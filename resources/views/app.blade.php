2f6bdece9f0b71e25ce4fc704482f994b5c3d52a
Francisco Perez <francisco@square1.io>
2020-10-21 13:06:25 -0400

    scaffolding

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Kanban</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    </head>
+    <body class="bg-white">
+        <div class="relative  overflow-hidden">
+           
+            <div class="text-center">
+                Hola Tailwind
+            </div>
+            
+        </div>
+    </body>
+</html>
